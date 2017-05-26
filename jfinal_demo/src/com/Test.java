23d7fbe68764c5f469797818ca15461ed8fd3a23
ICHWAO <896206565@qq.com>
2017-05-26 13:21:56 +0800

    提交测试

--- jfinal_demo/src/com/Test.java
+++ jfinal_demo/src/com/Test.java
@@ -19,7 +19,7 @@ public class Test {
 	
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		/*Date l_datetime = new Date();
+		Date l_datetime = new Date();
 		DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		TimeZone l_timezone = TimeZone.getTimeZone("GMT-0");
 		formatter.setTimeZone(l_timezone);
@@ -40,36 +40,7 @@ public class Test {
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
-		}*/
-		final int NMAX = 10; 
-		 
-	      // allocate triangular array
-	      int[][] odds = new int[NMAX + 1][];
-	      for (int n = 0; n <= NMAX; n++)
-	         odds[n] = new int[n + 1];  
-	 
-	      // fill triangular array
-	      for (int n = 0; n < odds.length; n++)
-	         for (int k = 0; k < odds[n].length; k++)
-	         {
-	            /*
-	             * compute binomial coefficient n*(n-1)*(n-2)*...*(n-k+1)/(1*2*3*...*k)
-	             */
-	            int lotteryOdds = 1;
-	            for (int i = 1; i <= k; i++)
-	               lotteryOdds = lotteryOdds * (n - i + 1) / i;
-	 
-	            odds[n][k] = lotteryOdds;
-	         }
-	 
-	      // print triangular array
-	      for (int[] row : odds)
-	      {
-	         for (int odd : row)
-	            System.out.printf("%4d", odd);
-	         System.out.println();
-	      }
-		
+		}
 	}
 
 	
